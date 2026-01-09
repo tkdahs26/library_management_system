892bb9f01b8889cbe835ee708b477a79e4352214
tkdahs26 <48806275+tkdahs26@users.noreply.github.com>
2026-01-09 11:53:10 +0900

    Fix: 대여 관련 오타 수정

--- library_system/main.py
+++ library_system/main.py
@@ -4,7 +4,7 @@ from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
+ 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'library.db')
 
